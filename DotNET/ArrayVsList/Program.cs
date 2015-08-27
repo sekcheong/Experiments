92a73b39df7b60c808332fffef145e1819c6e05b
Sek Cheong <scheong@gmail.com>
2015-08-26 20:46:30 -0500

    :cat2:

--- DotNET/ArrayVsList/Program.cs
+++ DotNET/ArrayVsList/Program.cs
@@ -219,3 +219,12 @@ static class Program
 		Console.ReadLine();
 	}
 }
+
+
+//long StopBytes = 0;
+//foo myFoo;
+
+//long StartBytes = System.GC.GetTotalMemory(true);
+//myFoo = new foo();
+//StopBytes = System.GC.GetTotalMemory(true);
+//GC.KeepAlive(myFoo); // This ensure a reference to object keeps object in memory
